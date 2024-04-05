64e8681e4c1477621aa1a0f4b0e05dd5d50cc0e4
Terry Holmes <tchaser3@outlook.com>
2024-04-05 07:22:48 -0400

    Add project files.

---
+++ OldEmployeesDLL/OldEmployeesClass.cs
@@ -0,0 +1,53 @@
+﻿/* Title:           Old Employees Dll
+ * Date:            4-25-17
+ * Author:          Terry Holmes
+ * 
+ * Description:     This class is for importing the old employees */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NewEventLogDLL;
+
+namespace OldEmployeesDLL
+{
+    public class OldEmployeesClass
+    {
+        //setting up the classes
+        EventLogClass TheEventLogClass = new EventLogClass();
+
+        //setting up the data
+        OldEmployeesDataSet aOldEmployeesDataSet;
+        OldEmployeesDataSetTableAdapters.employeesTableAdapter aOldEmployeesTableAdapter;
+
+        public OldEmployeesDataSet GetOldEmployeesInfo()
+        {
+            try
+            {
+                aOldEmployeesDataSet = new OldEmployeesDataSet();
+                aOldEmployeesTableAdapter = new OldEmployeesDataSetTableAdapters.employeesTableAdapter();
+                aOldEmployeesTableAdapter.Fill(aOldEmployeesDataSet.employees);
+            }
+            catch (Exception Ex)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "Old Employee Class // Get Old Employee Info " + Ex.Message);
+            }
+
+            return aOldEmployeesDataSet;
+        }
+        public void UpdateOldEmployeesDB(OldEmployeesDataSet aOldEmployeesDataSet)
+        {
+            try
+            {
+                aOldEmployeesTableAdapter = new OldEmployeesDataSetTableAdapters.employeesTableAdapter();
+                aOldEmployeesTableAdapter.Update(aOldEmployeesDataSet.employees);
+            }
+            catch (Exception Ex)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "Old Employee Class // Update Old Employee DB " + Ex.Message);
+            }
+        }
+    }
+}
